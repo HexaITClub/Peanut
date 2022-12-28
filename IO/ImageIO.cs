f84527c053f5e893cf72634538ede84cbfed3a77
rigel-star <boyzdod@gmail.com>
2022-12-28 22:21:00 +0545

    IO related operations

---
+++ IO/ImageIO.cs
@@ -0,0 +1,13 @@
+﻿using Peanut.Graphics;
+
+namespace Peanut.IO
+{
+    public class ImageIO
+    {
+        public static bool Save(Image image, string path, string ext)
+        {
+            Console.WriteLine("Saving " + image + " as " + path);
+            return true;
+        }
+    }
+}
